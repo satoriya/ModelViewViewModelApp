dfa510578e253b4f3af5eb2d2e34c25144d14ed1
satoriya <satriya.wicaksana89@gmail.com>
2023-03-13 10:36:37 +0700

    Initial Commit

---
+++ ModelViewViewModelApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ModelViewViewModelApp
+//
+//  Created by Phincon on 13/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
